0c6afb3a2776fb4ccd6f986d949734d6bf55bd1e
angep72 <p.umunyana@alustudent.com>
2024-02-14 02:24:31 -0800

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a Fabric script that generates a .tgz archive from the contents of the web_static"""
+"""a Fabric script that generates a .tgz archive"""
 
 from datetime import datetime
 from fabric.api import local
